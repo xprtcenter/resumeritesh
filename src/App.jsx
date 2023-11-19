124a93b57a5291d793e8e297fc02566330a42f1b
xprtcenter <81644623+xprtcenter@users.noreply.github.com>
2023-11-19 15:57:17 +0530

    complete vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import IMAGES from "./assets/images";
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<>
+			<div className="main">
+				<div className="navbar">
+					<img src={IMAGES.logo} alt="logo" className="logo" />
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+					<ul>
+						<li>
+							<a href="#">HOME</a>
+						</li>
+						<li>
+							<a href="#">ABOUT</a>
+						</li>
+						<li>
+							<a href="#">SKILLS</a>
+						</li>
+						<li>
+							<a href="#">MY WORKS</a>
+						</li>
+						<li>
+							<a href="#">CONTACT</a>
+						</li>
+					</ul>
+				</div>
+
+				<div className="info">
+					<h3>
+						Hi, I'm <span>Ritesh Rastogi</span>.
+					</h3>
+					<h1>
+						<span>W</span>EB <span>D</span>ESIGNER
+					</h1>
+					<h3>
+						Web designer and developer from Uttar Pradesh,India. I create <br />
+						websites to do businesses do better <br />
+						online.
+					</h3>
+					<a href="https://drive.google.com/file/d/1leHmnABGh7j4nAj5ZLLMVafiVD8zqeVD/view?usp=sharing">
+						Resume Download
+					</a>
+				</div>
+
+				<div className="image">
+					<img src={IMAGES.ritesh} alt="ritesh" className="ritesh" />
+				</div>
+
+				<div className="icons">
+					<a href="#">
+						<ion-icon name="logo-facebook"></ion-icon>
+					</a>
+					<a href="#">
+						<ion-icon name="logo-instagram"></ion-icon>
+					</a>
+					<a href="#">
+						<ion-icon name="logo-twitter"></ion-icon>
+					</a>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/assets/images.jsx
@@ -0,0 +1,5 @@
+const IMAGES = {
+	ritesh: new URL("./ritesh.png", import.meta.url).href,
+	logo: new URL("./Xprtlogo.png", import.meta.url).href,
+};
+export default IMAGES;
